1adbd2c6f91559f65db1e584bfc17fab2e6e206e
rgsarmiento <rg891126@gmail.com>
2022-05-31 13:04:09 -0500

    cambio rg

--- Visions-R/Metodos/Variables.vb
+++ Visions-R/Metodos/Variables.vb
@@ -2,6 +2,8 @@
 
     Public Shared ruta_raiz As String = AppDomain.CurrentDomain.BaseDirectory
     Public Shared conexion_acceso As String = ""
+
+
     Public Shared costo_con_iva As Integer = 0
 
 End Class
